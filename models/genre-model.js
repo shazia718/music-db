a6ce7d56e86bff6ab0a27aa438c8a6bb29162801
Shazia Anjum <Shazia@C4Qs-MacBook-Pro-5.local>
2016-12-01 16:59:15 -0500

    model created

--- models/genre-model.js
+++ models/genre-model.js
@@ -4,6 +4,13 @@ const sequelizeConnection = require('../db');
 //////////
 // YOUR CODE HERE:
 //////////
+var Genre = sequelizeConnection.define('genre', {
+	title: { type: Sequelize.STRING,
+		  validate: { len: [1, 100], notEmpty: true }
 
+		
+
+	}
+})
 
 module.exports = Genre;
